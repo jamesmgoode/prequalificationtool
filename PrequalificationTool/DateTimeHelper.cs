be4e19382a8233921e945e97b0bfed0951da9d35
Goode, James <James.Goode@experian.com>
2017-11-28 12:55:02 +0000

    Added test project and datetime helper

---
+++ PrequalificationTool/DateTimeHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PrequalificationTool
+{
+    public interface IDateTimeHelper
+    {
+        DateTime Now();
+    }
+
+    public class DateTimeHelper : IDateTimeHelper
+    {
+        public virtual DateTime Now()
+        {
+            return DateTime.Now;
+        }
+    }
+}
---
+++ TestPrequalificationTool/TestApplicationProcessor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestPrequalificationTool
+{
+    [TestClass]
+    public class TestApplicationProcessor
+    {
+        [TestMethod]
+        public void TestAgeUnder18()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestAgeExactly18()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestAgeOver18()
+        {
+
+        }
+    }
+}
